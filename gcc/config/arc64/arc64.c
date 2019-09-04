94041e1b98b1b2b6135f63ebf3530434742327a7
Claudiu Zissulescu <claziss@gmail.com>
2019-09-04 18:35:52 +0300

    Fixes

--- gcc/config/arc64/arc64.c
+++ gcc/config/arc64/arc64.c
@@ -98,7 +98,7 @@ void arc64_init_expanders (void)
    COMPARE, return the mode to be used for the comparison.  */
 
 machine_mode
-arc64_select_cc_mode (RTX_CODE op, rtx x, rtx y)
+arc64_select_cc_mode (enum rtx_code op, rtx x, rtx y)
 {
   return CC_Cmode;
 }
@@ -339,6 +339,14 @@ arc64_can_use_return_insn_p (void)
   return (reload_completed); //FIXME!: && cfun->machine->frame.frame_size == 0);
 }
 
+/* This is how to output a definition of an internal numbered label where
+   PREFIX is the class of label and NUM is the number within the class.  */
+
+static void arc_internal_label (FILE *stream, const char *prefix, unsigned long labelno)
+{
+  default_internal_label (stream, prefix, labelno);
+}
+
 /* Target hooks.  */
 
 #undef TARGET_ASM_ALIGNED_DI_OP
@@ -365,6 +373,9 @@ arc64_can_use_return_insn_p (void)
 
 #undef TARGET_C_MODE_FOR_FLOATING_TYPE
 #define TARGET_C_MODE_FOR_FLOATING_TYPE arc64_c_mode_for_floating_type
+
+#undef TARGET_ASM_INTERNAL_LABEL
+#define TARGET_ASM_INTERNAL_LABEL arc64_internal_label
 
 struct gcc_target targetm = TARGET_INITIALIZER;
 
