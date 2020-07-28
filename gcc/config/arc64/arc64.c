e2a10e0f5e06480a6cf0571af0fa901c47de44ec
Claudiu Zissulescu <claziss@gmail.com>
2020-07-28 20:12:28 +0300

    arc64: Add R30 as epilogue used when TLS
    Signed-off-by: Claudiu Zissulescu <claziss@gmail.com>

--- gcc/config/arc64/arc64.c
+++ gcc/config/arc64/arc64.c
@@ -2148,6 +2148,11 @@ arc64_can_use_return_insn_p (void)
 int
 arc64_epilogue_uses (int regno)
 {
+#ifdef HAVE_AS_TLS
+  if (regno == R30_REGNUM)
+    return 1;
+#endif
+
   if (epilogue_completed)
     if (regno == BLINK_REGNUM)
       return 1;
