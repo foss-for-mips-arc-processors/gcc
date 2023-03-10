2340907a6f13ac7ea53d84eaf8de962bf92f3189
Claudiu Zissulescu <claziss@gmail.com>
2023-03-10 13:08:18 +0200

    arcv: Add initial scheduling scheme.
    Signed-off-by: Claudiu Zissulescu <claziss@gmail.com>

--- gcc/config/riscv/riscv-opts.h
+++ gcc/config/riscv/riscv-opts.h
@@ -58,6 +58,7 @@ enum riscv_microarchitecture_type {
   sifive_p400,
   sifive_p600,
   xiangshan,
+  arcv_rhx100,
   generic_ooo
 };
 extern enum riscv_microarchitecture_type riscv_microarchitecture;
