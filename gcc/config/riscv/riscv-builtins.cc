14b9cd644697b37ddfd99b60213779f532637cae
Luis Silva <luiss@synopsys.com>
2025-06-23 03:12:15 -0700

    arcv: apex: set APEX operand flags based on function prototype.
    Adds `arcv_apex_set_insn_operand_flags`, which examines the return type
    and arguments of a built-in function to determine the appropriate APEX
    operand flags.  A non-void return type sets APEX_DEST, and up to two
    arguments set APEX_SRC0 and APEX_SRC1.
    
    Emits an error if the function has more than two arguments.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- gcc/config/riscv/riscv-builtins.cc
+++ gcc/config/riscv/riscv-builtins.cc
@@ -480,3 +480,92 @@ riscv_atomic_assign_expand_fenv (tree *hold, tree *clear, tree *update)
   *clear = build_call_expr (fsflags, 1, old_flags);
   *update = NULL_TREE;
 }
+
+/* Set APEX operand flags for a built-in function.
+   This function inspects the function prototype in FNDECL and sets the
+   appropriate operand flags in INSN_FORMAT:
+     - APEX_DEST if the return type is not void.
+     - APEX_SRC0 and/or APEX_SRC1 depending on the number of arguments.
+   Emits an error if more than 2 arguments are present.  */
+
+static unsigned int
+arcv_apex_set_insn_operand_flags (unsigned int insn_format, tree fndecl)
+{
+  /* Set DEST flag if the function does not return void.  */
+  tree return_type = TREE_TYPE (TREE_TYPE (fndecl));
+  if (return_type != void_type_node)
+    insn_format |= APEX_DEST;
+
+  /* Count non‑void parameters, aborting if there are more than two.  */
+  unsigned int nargs = 0;
+  for (tree arg = TYPE_ARG_TYPES (TREE_TYPE (fndecl));
+       arg && TREE_CODE (TREE_VALUE (arg)) != VOID_TYPE;
+       arg = TREE_CHAIN (arg))
+  {
+    if (++nargs > 2)
+    {
+      warning (0, "pragma intrinsic: Associated function can have "
+	"no more than 2 parameters");
+      return 0xFFFFFFFF;
+    }
+
+    /* Only perform size checks on 32-bit architectures.  */
+    if (POINTER_SIZE != 32)
+      continue;
+
+    /* We want to check the size of the value represented by the argument.
+       - If it's a pointer, we check the size of the pointed-to type.
+       - If it's a scalar or aggregate type, we check its own size.  */
+    tree argtype = TREE_VALUE (arg);
+    tree type_to_check = argtype;
+    if (TREE_CODE (argtype) == POINTER_TYPE)
+    {
+      /* Skip size check if return type is void.  */
+      if (return_type == void_type_node)
+	continue;
+
+      type_to_check = TREE_TYPE (argtype);
+    }
+    else
+      type_to_check = argtype;
+
+    /* If TYPE_SIZE_UNIT exists and represents a constant integer value,
+       retrieve its size in bytes as a HOST_WIDE_INT.  */
+    if (! (TYPE_SIZE_UNIT (type_to_check)
+	&& tree_fits_uhwi_p (TYPE_SIZE_UNIT (type_to_check))))
+      continue;
+
+    HOST_WIDE_INT bytes = tree_to_uhwi (TYPE_SIZE_UNIT (type_to_check));
+
+    /* If the type’s size is greater than 4 bytes, emit an error.
+       This applies to both pointed-to types and scalar types
+       larger than 4 bytes.  */
+    if (bytes <= 4)
+      continue;
+
+    const char *fn_name = IDENTIFIER_POINTER (DECL_NAME (fndecl));
+    if (TREE_CODE (argtype) == POINTER_TYPE)
+    {
+      /* Specific error for pointer parameters when return type is not void.  */
+      error ("pragma intrinsic: APEX function %qs must return "
+		"void or a scalar type that does not exceed 4 bytes",
+		fn_name);
+    }
+    else
+    {
+      /* General error for large or non-scalar parameter types.  */
+      error ("pragma intrinsic: APEX function %qs contains a parameter "
+		"of a non-scalar type, or one that exceeds 4 bytes",
+		fn_name);
+    }
+    return 0xFFFFFFFF;
+  }
+
+  /* Source‑operand flags.  */
+  if (nargs >= 1)
+    insn_format |= APEX_SRC0;
+  if (nargs == 2)
+    insn_format |= APEX_SRC1;
+
+  return insn_format;
+}
