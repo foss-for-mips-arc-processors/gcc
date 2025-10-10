533a77f669ddbf16c70434872891841290139ad4
Luis Silva <luiss@synopsys.com>
2025-10-10 13:16:01 +0100

    arcv: Fix missing vsetvli generation for arcv_vmv instructions.
    The arcv_vmv RTL patterns were not generating vsetvli instructions
    because they lacked the proper RVV predication structure.  This caused
    the vector configuration to not be set up before executing the
    arcv.vmv.v.s and arcv.vmv.s.v instructions.
    
    This patch restructures the arcv_vmv patterns to follow the standard
    RVV predicated instruction format by:
    - Wrapping the instruction in an if_then_else with UNSPEC_VPREDICATE.
    - Adding VL_REGNUM and VTYPE_REGNUM registers to the predication.
    - Moving the actual instruction logic to an inner unspec.
    
    This structure matches other ARCV vector instructions (like vnorm)
    and enables the vsetvl pass to properly insert vsetvli instructions
    before the arcv_vmv operations.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- gcc/testsuite/gcc.target/riscv/arcv-vdsp-vmvi_s_v-compile-1.c
+++ gcc/testsuite/gcc.target/riscv/arcv-vdsp-vmvi_s_v-compile-1.c
@@ -10,6 +10,7 @@
 
 /*
 ** test_vmv_s_v_i8m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m1,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -20,6 +21,7 @@ vint8m1_t test_vmv_s_v_i8m1 (vint8m1_t vd, int vs1, vint8m1_t vs2, size_t vl)
 
 /*
 ** test_vmv_s_v_i8m2:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m2,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -30,6 +32,7 @@ vint8m1_t test_vmv_s_v_i8m2 (vint8m1_t vd, int vs1, vint8m2_t vs2, size_t vl)
 
 /*
 ** test_vmv_s_v_i8m4:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m4,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -40,6 +43,7 @@ vint8m1_t test_vmv_s_v_i8m4 (vint8m1_t vd, int vs1, vint8m4_t vs2, size_t vl)
 
 /*
 ** test_vmv_s_v_i16m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e16,m1,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -50,6 +54,7 @@ vint16m1_t test_vmv_s_v_i16m1 (vint16m1_t vd, int vs1, vint16m1_t vs2, size_t vl
 
 /*
 ** test_vmv_s_v_i32m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e32,m1,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -60,6 +65,7 @@ vint32m1_t test_vmv_s_v_i32m1 (vint32m1_t vd, int vs1, vint32m1_t vs2, size_t vl
 
 /*
 ** test_vmv_s_v_i32m2:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e32,m2,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
@@ -70,6 +76,7 @@ vint32m1_t test_vmv_s_v_i32m2 (vint32m1_t vd, int vs1, vint32m2_t vs2, size_t vl
 
 /*
 ** test_vmv_s_v_i64m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,m1,\s*t[au],\s*m[au]
 **  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
