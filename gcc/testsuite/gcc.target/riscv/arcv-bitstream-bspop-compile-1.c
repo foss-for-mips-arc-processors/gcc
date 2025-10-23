6e0e72eca3fb7bafd6960d4d9e95d45b9ed1e486
Michiel Derhaeg <michiel@synopsys.com>
2025-10-23 15:39:02 +0200

    arcv: Don't make xarcv imply v
    'v' implies 'zvl128b' and 'zve64d' while ARC-V hardware might be
    configured to support smaller VLEN's or no FPU support.
    
    - xarcvbit(rev|stream) are not RVV intrinsics
    - other xarcv extensions now imply 'zve32x', the "smallest" RVV extension

--- gcc/testsuite/gcc.target/riscv/arcv-bitstream-bspop-compile-1.c
+++ gcc/testsuite/gcc.target/riscv/arcv-bitstream-bspop-compile-1.c
@@ -3,9 +3,10 @@
 /* { dg-options "-march=rv32im_xarcvbitstream -mabi=ilp32" } */
 
 #include <stddef.h>
-#include <riscv_vector.h>
 
-int test_bspop (int vs2) {
-  return __builtin_riscv_arcv_bspop (vs2); }
+int test_bspop (int vs2)
+{
+  return __builtin_riscv_arcv_bspop (vs2);
+}
 
 /* { dg-final { scan-assembler-times "arcv\\.bspop" 1 } } */
