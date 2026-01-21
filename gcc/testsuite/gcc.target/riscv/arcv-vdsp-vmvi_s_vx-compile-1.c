58f2db54bf730aa7a6714f547ce55c583946651e
Luis Silva <luiss@synopsys.com>
2026-01-21 11:12:40 +0000

    tmp: arcv-vdsp-vmvi_s_vx-compile-1.c
    Signed-off-by: Luis Silva <luiss@synopsys.com>

---
+++ gcc/testsuite/gcc.target/riscv/arcv-vdsp-vmvi_s_vx-compile-1.c
@@ -0,0 +1,86 @@
+/* { dg-do compile } */
+/* { dg-require-effective-target arcv_vdsp } */
+/* { dg-skip-if "" { *-*-* } { "-O0" "-O1" "-O3" "-Os" "-Og" "-Oz" "-flto" } } */
+/* { dg-options "-march=rv32imv_xarcvvdsp -mabi=ilp32 -O2" } */
+/* { dg-final { check-function-bodies "**" "" } } */
+
+#include <stddef.h>
+#include <riscv_vector.h>
+
+
+/*
+** test_vmv_s_vx_i8m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m1,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint8m1_t test_vmv_s_vx_i8m1 (vint8m1_t vd, int vs1, vint8m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i8m1_i8m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i8m2:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m2,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint8m1_t test_vmv_s_vx_i8m2 (vint8m1_t vd, int vs1, vint8m2_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i8m2_i8m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i8m4:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e8,m4,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint8m1_t test_vmv_s_vx_i8m4 (vint8m1_t vd, int vs1, vint8m4_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i8m4_i8m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i16m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e16,m1,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint16m1_t test_vmv_s_vx_i16m1 (vint16m1_t vd, int vs1, vint16m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i16m1_i16m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i32m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e32,m1,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint32m1_t test_vmv_s_vx_i32m1 (vint32m1_t vd, int vs1, vint32m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i32m1_i32m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i32m2:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e32,m2,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint32m1_t test_vmv_s_vx_i32m2 (vint32m1_t vd, int vs1, vint32m2_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i32m2_i32m1 (vd, 1, vs2, vl);
+}
+
+/*
+** test_vmv_s_vx_i64m1:
+**  vsetvli\s+zero,\s*[a-x0-9]+,\s*e64,m1,\s*t[au],\s*m[au]
+**  arcv\.vmvi\.s\.v\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint64m1_t test_vmv_s_vx_i64m1 (vint64m1_t vd, int vs1, vint64m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vmv_s_vx_i64m1_i64m1 (vd, 1, vs2, vl);
+}
