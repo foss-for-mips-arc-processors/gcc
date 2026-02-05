1b42ea5ec9680b6106ecb20da9ca4042071110a0
Luis Silva <luiss@synopsys.com>
2026-02-05 01:24:43 -0800

    arcv: Fix arcv.vclr intrinsic signature.
    According to the XARCV 1.8 specification, he arcv.vclr instruction takes
    (imm, vl) in the base variant and (vector, imm, vl) for tail-undisturbed.
    Test case updated to remove the vector input in base variant, remove mask
    intrinsic and add tail-undisturbed.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- gcc/testsuite/gcc.target/riscv/arcv-vdsp-vclr_v_i-compile-1.c
+++ gcc/testsuite/gcc.target/riscv/arcv-vdsp-vclr_v_i-compile-1.c
@@ -9,89 +9,89 @@
 
 
 /*
-** test_vclr_v_i_i8:
-**  vsetivli	zero,1,e8,m1,ta,ma
+** test_vclr_v_i8:
+**  vsetvli	zero,a0,e8,m1,ta,ma
 **  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
-vint8m1_t test_vclr_v_i_i8 (vint8m1_t vs2, size_t vl)
+vint8m1_t test_vclr_v_i8 (size_t vl)
 {
-  return __riscv_arcv_vclr_v_i_i8m1 (vs2, 1, vl);
+  return __riscv_arcv_vclr_v_i8m1 (1, vl);
 }
 
 /*
-** test_vclr_v_i_i8_m:
-**  vsetivli	zero,1,e8,m1,ta,ma
-**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1,\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1])\.t
-**  ret
-*/
-vint8m1_t test_vclr_v_i_i8_m (vbool8_t mask, vint8m1_t vs2, size_t vl)
-{
-  return __riscv_arcv_vclr_v_i_i8m1_m (mask, vs2, 1, vl);
-}
-
-/*
-** test_vclr_v_i_i16:
-**  vsetivli	zero,1,e16,m1,ta,ma
+** test_vclr_v_i8_tu:
+**  vsetvli	zero,a0,e8,m1,tu,ma
 **  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
-vint16m1_t test_vclr_v_i_i16 (vint16m1_t vs2, size_t vl)
+vint8m1_t test_vclr_v_i8_tu (vint8m1_t vs2, size_t vl)
 {
-  return __riscv_arcv_vclr_v_i_i16m1 (vs2, 1, vl);
+  return __riscv_arcv_vclr_v_i8m1_tu (vs2, 1, vl);
 }
 
 /*
-** test_vclr_v_i_i16_m:
-**  vsetivli	zero,1,e16,m1,ta,ma
-**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1,\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1])\.t
-**  ret
-*/
-vint16m1_t test_vclr_v_i_i16_m (vbool16_t mask, vint16m1_t vs2, size_t vl)
-{
-  return __riscv_arcv_vclr_v_i_i16m1_m (mask, vs2, 1, vl);
-}
-
-/*
-** test_vclr_v_i_i32:
-**  vsetivli	zero,1,e32,m1,ta,ma
+** test_vclr_v_i16:
+**  vsetvli	zero,a0,e16,m1,ta,ma
 **  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
-vint32m1_t test_vclr_v_i_i32 (vint32m1_t vs2, size_t vl)
+vint16m1_t test_vclr_v_i16 (size_t vl)
 {
-  return __riscv_arcv_vclr_v_i_i32m1 (vs2, 1, vl);
+  return __riscv_arcv_vclr_v_i16m1 (1, vl);
 }
 
 /*
-** test_vclr_v_i_i32_m:
-**  vsetivli	zero,1,e32,m1,ta,ma
-**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1,\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1])\.t
-**  ret
-*/
-vint32m1_t test_vclr_v_i_i32_m (vbool32_t mask, vint32m1_t vs2, size_t vl)
-{
-  return __riscv_arcv_vclr_v_i_i32m1_m (mask, vs2, 1, vl);
-}
-
-/*
-** test_vclr_v_i_i64:
-**  vsetivli	zero,1,e64,m1,ta,ma
+** test_vclr_v_i16_tu:
+**  vsetvli	zero,a0,e16,m1,tu,ma
 **  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
-vint64m1_t test_vclr_v_i_i64 (vint64m1_t vs2, size_t vl)
+vint16m1_t test_vclr_v_i16_tu (vint16m1_t vs2, size_t vl)
 {
-  return __riscv_arcv_vclr_v_i_i64m1 (vs2, 1, vl);
+  return __riscv_arcv_vclr_v_i16m1_tu (vs2, 1, vl);
 }
 
 /*
-** test_vclr_v_i_i64_m:
-**  vsetivli	zero,1,e64,m1,ta,ma
-**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1,\s*(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1])\.t
+** test_vclr_v_i32:
+**  vsetvli	zero,a0,e32,m1,ta,ma
+**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
 **  ret
 */
-vint64m1_t test_vclr_v_i_i64_m (vbool64_t mask, vint64m1_t vs2, size_t vl)
+vint32m1_t test_vclr_v_i32 (size_t vl)
 {
-  return __riscv_arcv_vclr_v_i_i64m1_m (mask, vs2, 1, vl);
+  return __riscv_arcv_vclr_v_i32m1 (1, vl);
+}
+
+/*
+** test_vclr_v_i32_tu:
+**  vsetvli	zero,a0,e32,m1,tu,ma
+**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint32m1_t test_vclr_v_i32_tu (vint32m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vclr_v_i32m1_tu (vs2, 1, vl);
+}
+
+/*
+** test_vclr_v_i64:
+**  vsetvli	zero,a0,e64,m1,ta,ma
+**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint64m1_t test_vclr_v_i64 (size_t vl)
+{
+  return __riscv_arcv_vclr_v_i64m1 (1, vl);
+}
+
+/*
+** test_vclr_v_i64_tu:
+**  vsetvli	zero,a0,e64,m1,tu,ma
+**  arcv\.vclr\.v\.i\s+(?:v[0-9]|v[1-2][0-9]|[a-x0-9]+[0-1]),\s*1
+**  ret
+*/
+vint64m1_t test_vclr_v_i64_tu (vint64m1_t vs2, size_t vl)
+{
+  return __riscv_arcv_vclr_v_i64m1_tu (vs2, 1, vl);
 }
