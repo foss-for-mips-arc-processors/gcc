7aebc414147c98cee5e589b54078158962468b8d
Michiel Derhaeg <michiel@synopsys.com>
2026-04-02 12:40:43 +0200

    fixup! arcv: add scheduling implementation for the Synopsys RPX-100 core

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -642,28 +642,6 @@ static const struct riscv_tune_param xiangshan_nanhu_tune_info = {
   true,						/* prefer-agnostic.  */
 };
 
-/* Costs to use when optimizing for Synopsys RPX-100.  */
-static const struct riscv_tune_param arcv_rpx100_tune_info = {
-  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_add */
-  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_mul */
-  {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
-  {COSTS_N_INSNS (4), COSTS_N_INSNS (4)},	/* int_mul */
-  {COSTS_N_INSNS (27), COSTS_N_INSNS (43)},	/* int_div */
-  4,						/* issue_rate */
-  9,						/* branch_cost */
-  2,						/* memory_cost */
-  8,						/* fmv_cost */
-  false,					/* slow_unaligned_access */
-  false,					/* vector_unaligned_access */
-  false,					/* use_divmod_expansion */
-  false,					/* overlap_op_by_pieces */
-  RISCV_FUSE_ARCV,				/* fusible_ops */
-  NULL,						/* vector cost */
-  NULL,						/* function_align */
-  NULL,						/* jump_align */
-  NULL,						/* loop_align */
-};
-
 /* Costs to use when optimizing for a generic ooo profile.  */
 static const struct riscv_tune_param generic_ooo_tune_info = {
   {COSTS_N_INSNS (2), COSTS_N_INSNS (2)},	/* fp_add */
@@ -915,6 +893,31 @@ static const struct riscv_tune_param arcv_rmx500_tune_info = {
 
 /* Costs to use when optimizing for Synopsys RHX-100.  */
 static const struct riscv_tune_param arcv_rhx100_tune_info = {
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_add */
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_mul */
+  {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
+  {COSTS_N_INSNS (4), COSTS_N_INSNS (4)},	/* int_mul */
+  {COSTS_N_INSNS (27), COSTS_N_INSNS (43)},	/* int_div */
+  4,						/* issue_rate */
+  9,						/* branch_cost */
+  2,						/* memory_cost */
+  8,						/* fmv_cost */
+  false,					/* slow_unaligned_access */
+  false,					/* vector_unaligned_access */
+  false,					/* use_divmod_expansion */
+  false,					/* overlap_op_by_pieces */
+  true,						/* use_zero_stride_load */
+  false,					/* speculative_sched_vsetvl */
+  RISCV_FUSE_ARCV,				/* fusible_ops */
+  NULL,						/* vector cost */
+  NULL,						/* function_align */
+  NULL,						/* jump_align */
+  NULL,						/* loop_align */
+  true,						/* prefer-agnostic.  */
+};
+
+/* Costs to use when optimizing for Synopsys RPX-100.  */
+static const struct riscv_tune_param arcv_rpx100_tune_info = {
   {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_add */
   {COSTS_N_INSNS (4), COSTS_N_INSNS (5)},	/* fp_mul */
   {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
