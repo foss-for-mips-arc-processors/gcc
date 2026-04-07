9bae699c8cb290519d6d7d6cd61024c31bc2ee81
Loeka Rogge <loeka@synopsys.com>
2026-04-07 05:31:48 -0700

    test dg linting

--- gcc/testsuite/gcc.target/arc/ashrsi-2.c
+++ gcc/testsuite/gcc.target/arc/ashrsi-2.c
@@ -1,8 +1,8 @@
 /* { dg-do compile } */
 /* { dg-skip-if "avoid conflicts with -mcpu options" { *-*-* } { "-mcpu=*" } { "-mcpu=em" } } */
-/* { dg-options "-O2 -mcpu=em" } */
+/* { dg-options "-O2 -mcpu=em" */
 
 int foo(int x) { return x >> 1; }
 
-/* { dg-final { scan-assembler-times "asr_s\\s+r0,r0" 1 } } */
+/* { dg-final scan-assembler-times "asr_s\\s+r0,r0" 1 } } */
 /* { dg-final { scan-assembler "j_s\.d" } } */
