f333029ac755d0f85748242edb2fffb48251b28c
Luis Silva <luis.silva@globalfoundries.com>
2026-07-20 14:34:27 +0100

    fixup! fixup! RISC-V: Implement TARGET_SCHED_FUSION_PRIORITY for ARC-V RHX-100 series.
    Signed-off-by: Luis Silva <luis.silva@globalfoundries.com>

--- gcc/testsuite/gcc.target/riscv/rvv/base/spill-10.c
+++ gcc/testsuite/gcc.target/riscv/rvv/base/spill-10.c
@@ -1,5 +1,5 @@
 /* { dg-do compile } */
-/* { dg-options "-march=rv32gcv -mabi=ilp32 -mpreferred-stack-boundary=3 -fno-schedule-insns -fno-schedule-insns2 -O3 -fno-unroll-loops" } */
+/* { dg-options "-march=rv32gcv -mabi=ilp32 -mpreferred-stack-boundary=3 -fno-schedule-insns -fno-schedule-insns2 -fno-schedule-fusion -O3 -fno-unroll-loops" } */
 /* { dg-final { check-function-bodies "**" "" } } */
 
 #include "riscv_vector.h"
